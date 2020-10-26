a897843edc7088ae97f1773df2a883750982af1b
ynswtf <yunussenem@icloud.com>
2020-10-26 18:36:13 +0300

    BoxKit Created

---
+++ scripts.php
@@ -0,0 +1,18 @@
+<?php 
+return [
+    'install' => function($app)
+    {
+        $util = $app['db']->getUtility();
+    },
+
+    'uninstall' => function($app)
+    {
+        $util = $app['db']->getUtility();
+    },
+
+    'enable' => function($app)
+    {
+        $util = $app['db']->getUtility();
+    }
+];
+?>
